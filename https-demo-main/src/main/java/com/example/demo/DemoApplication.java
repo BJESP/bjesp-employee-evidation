7f4fb683eb0eb9c171b3272b0b0a4f866941216d
Kumici13 <lukapikula13@gmail.com>
2023-05-14 12:40:43 +0200

    mail sent

--- https-demo-main/src/main/java/com/example/demo/DemoApplication.java
+++ https-demo-main/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.service.EmailService;
+import jakarta.mail.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Properties;
+
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DemoApplication.class, args);
+		EmailService em = new EmailService();
+		em.SendPasswordlessLoginEmail("lukapikula13+1@gmail.com");
 	}
-
 }
