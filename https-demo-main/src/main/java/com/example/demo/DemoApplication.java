4d5f69eadc37e0c936c3748bd6b3b0cb2cf4bc2c
Kumici13 <lukapikula13@gmail.com>
2023-05-14 15:52:25 +0200

    Update DemoApplication.java

--- https-demo-main/src/main/java/com/example/demo/DemoApplication.java
+++ https-demo-main/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.service.EmailService;
-import jakarta.mail.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Properties;
-
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DemoApplication {
