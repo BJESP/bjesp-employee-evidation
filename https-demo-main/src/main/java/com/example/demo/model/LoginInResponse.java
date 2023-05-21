d52aaf8740bbbb0c7ac6ce277a201ee03262c787
cikii555 <cvetanaciki555@gmail.com>
2023-05-21 23:41:01 +0200

    implemented some logic for RBAC and adapted other function to it

--- https-demo-main/src/main/java/com/example/demo/model/LoginInResponse.java
+++ https-demo-main/src/main/java/com/example/demo/model/LoginInResponse.java
@@ -9,15 +9,49 @@ public class LoginInResponse {
     private Long id;
     private String username;
     private String email;
-    //private List<String> roles;
+    private List<String> roles;
 
-    public LoginInResponse(String accessToken, String refreshToken, Long id,  String email) {
+    public LoginInResponse(String accessToken, String refreshToken, Long id,  String email,List<String>roles) {
         this.token = accessToken;
         this.refreshToken = refreshToken;
         this.id = id;
-
+        this.roles = roles;
         this.email = email;
 
+    }
+
+    public LoginInResponse(String token, String type, String refreshToken, Long id, String username, String email, List<String> roles) {
+        this.token = token;
+        this.type = type;
+        this.refreshToken = refreshToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 
     public String getAccessToken() {
